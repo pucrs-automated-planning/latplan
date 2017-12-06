8f6bb513287c392f525d4c447e340d312722f557
Leonardo Amado <leonardo.amado@acad.pucrs.br>
2017-12-06 11:36:04 -0200

    Initial code cleanup

--- generate_domain.py
+++ generate_domain.py
@@ -2,57 +2,9 @@ from action import *
 #Latent layer size
 N = 36
 
-def generate_action(state1, state2):
-	parameter = _or(state1,state2) 
-	pre_cond = _xnor(state1,state1)
-	xor = _xor(state1,state2)
-	effect = [0] * len(state1)
-	for i in range(len(xor)):
-		if not xor[i]: continue
-		if ((state1[i] == 1) and (xor[i] == 1)): 
-			effect[i] = -1
-		else: 
-			effect[i] = 1 
-	return parameter, pre_cond, effect
 
-#Deprecated
-def generate_ppdl_action_old(parameter, pre_cond, effect, action_name):
-	action = '(:action ' + str(action_name) + '\n'
-	action += '    :parameters ('
-	for par in range(len(parameter)):
-		if abs(parameter[par]): action+= '?v' + str(par) + ' '
-	action += ')'+ '\n'
-	action += '    :precondition (and' + '\n'
-	for pre in range(len(pre_cond)):
-		if pre_cond[pre] == 1:  action+='        ('+ 'p' + str(pre) + ' ?v'+ str(pre) + ')' +'\n'
-		if pre_cond[pre] == -1:  action+='        (not ('+ 'p' + str(pre) + ' ?v'+ str(pre) + '))' +'\n'
-	action  += '    )' +'\n'
-	action += '    :effect(and' + '\n'
-	for eff in range(len(effect)):
-		if not effect[eff]: continue
-		if effect[eff] == 1: action += '        ('+ 'p' + str(eff) + ' ?v'+ str(eff) + ')' +'\n'
-		if effect[eff] == -1: action += '        (not ('+ 'p' + str(eff) + ' ?v'+ str(eff) + '))' +'\n'
-	action += '    )' +'\n'
-	action += ')\n'
-	return action
 
-def generate_ppdl_action(parameter, pre_cond, effect, action_name):
-	action = '(:action ' + str(action_name) + '\n'
-	action += '    :parameters ()\n'
-	action += '    :precondition (and' + '\n'
-	for pre in range(len(pre_cond)):
-		if pre_cond[pre] == 1:  action+='        ('+ 'p' + str(pre) +')' +'\n'
-		if pre_cond[pre] == -1:  action+='        (not ('+ 'p' + str(pre) +'))' +'\n'
-	action  += '    )' +'\n'
-	action += '    :effect(and' + '\n'
-	for eff in range(len(effect)):
-		if not effect[eff]: continue
-		if effect[eff] == 1: action += '        ('+ 'p' + str(eff) + ')' +'\n'
-		if effect[eff] == -1: action += '        (not ('+ 'p' + str(eff) + '))' +'\n'
-	action += '    )' +'\n'
-	action += ')\n'
-	return action
-
+#============ LOGIC OPERATORS ==============
 def _or(self,other):
         return [se | so for se,so in zip(self,other)]   
 
@@ -71,6 +23,41 @@ def sxnor(se,so):
 	if (se == -1) and (so == -1):
 		return -1
 	return 0 
+
+#===========================================
+#============ ACTION MANAGEMENT ============
+def generate_action(state1, state2):
+	parameter = _or(state1,state2) 
+	pre_cond = _xnor(state1,state1)
+	xor = _xor(state1,state2)
+	effect = [0] * len(state1)
+	for i in range(len(xor)):
+		if not xor[i]: continue
+		if ((state1[i] == 1) and (xor[i] == 1)): 
+			effect[i] = -1
+		else: 
+			effect[i] = 1 
+	return parameter, pre_cond, effect
+
+
+
+def generate_ppdl_action(parameter, pre_cond, effect, action_name):
+	action = '(:action ' + str(action_name) + '\n'
+	action += '    :parameters ()\n'
+	action += '    :precondition (and' + '\n'
+	for pre in range(len(pre_cond)):
+		if pre_cond[pre] == 1:  action+='        ('+ 'p' + str(pre) +')' +'\n'
+		if pre_cond[pre] == -1:  action+='        (not ('+ 'p' + str(pre) +'))' +'\n'
+	action  += '    )' +'\n'
+	action += '    :effect(and' + '\n'
+	for eff in range(len(effect)):
+		if not effect[eff]: continue
+		if effect[eff] == 1: action += '        ('+ 'p' + str(eff) + ')' +'\n'
+		if effect[eff] == -1: action += '        (not ('+ 'p' + str(eff) + '))' +'\n'
+	action += '    )' +'\n'
+	action += ')\n'
+	return action
+
 
 def generate_all_actions_pddl(list_actions):
 	actions = []
@@ -91,6 +78,9 @@ def generate_all_actions(list_actions):
 		actions_set.add(a)
 	return actions_set
 
+#===========================================
+#============ PDDL OUTPUT ==================
+
 def export_pddl(actions, path):
 	txt = '(define (domain generated-domain) \n'
   	txt += '    (:requirements :strips :negative-preconditions) \n'
@@ -104,19 +94,7 @@ def export_pddl(actions, path):
 	data = open(path, 'wb')
 	data.write(txt)
 
-#Deprecated
-def export_pddl_old(actions, path):
-	txt = '(define (domain generated-domain) \n'
-  	txt += '    (:requirements :strips) \n'
-  	txt += '    (:predicates \n'
-  	for i in range(N):
-  		txt += '        (p' +str(i) + ' ?v' + str(i) + ') \n'
-  	txt += '    )\n'
-  	for action in actions:
-  		txt += action
-  	txt += ')'
-	data = open(path, 'wb')
-	data.write(txt)
+
 
 def generate_problem(init_state, goal_state):
 	txt = '(define (problem pb1)\n'
@@ -136,28 +114,6 @@ def generate_problem(init_state, goal_state):
 	txt += '    )\n)'
 	return txt
 
-#Deprecated
-def generate_problem_old(init_state, goal_state):
-	txt = '(define (problem pb1)\n'
- 	txt += '    (:domain generated-domain)\n'
-   	txt += '    (:requirements :strips :negative-preconditions)'
-  	txt += '    (:objects'
-  	for i in range(N):
-  		txt += '        o' + str(i) + '\n'
-  	txt += '    )\n'
-	txt += '    (:init\n'
-	for pre in range(len(init_state)):
-		if init_state[pre]:  txt+='       ('+ 'p' + str(pre) + ' o'+ str(pre) + ')' +'\n'
-		else: txt+='       (not ('+ 'p' + str(pre) + ' o'+ str(pre) + '))' +'\n'
-	txt += '    )\n'
-	txt += '    (:goal\n'
-	txt += '      (and\n'
-	for pre in range(len(goal_state)):
-		if goal_state[pre]:  txt+='       ('+ 'p' + str(pre) + ' o'+ str(pre) + ')' +'\n'
-		else: txt+='       (not ('+ 'p' + str(pre) + ' o'+ str(pre) + '))' +'\n'
-	txt += '      )\n'
-	txt += '    )\n)'
-	return txt
 
 def read_csv_actions(path):
 	data = open(path, 'r')
@@ -169,6 +125,9 @@ def read_csv_actions(path):
 		s2 = line[len(line)/2:]
 		actions.append((s1,s2))
 	return actions
+
+#===========================================
+#========= PRUNING AND EXECUTION ===========
 
 def prune_actions(actions):
 	meta_actions = []
@@ -217,11 +176,11 @@ def check_action(actions, state_1, state_2):
 		if a.effect == eff:
 			print 'Found', a.effect
 
-#generate_ppdl_action(p,pre,eff,'ac1')
+#===========================================
+#============ MODULES AND MAIN =============
 
-#export_pddl(generate_all_actions([([0,0,1,0,0,1], [1,0,0,0,1,1]), ( [1,0,0,0,1,1], [0,0,1,0,0,1])]), 'domain.pddl')
 def create_domain():
-	transitions = read_csv_actions('samples/puzzle_mnist_3_3_36_20000_conv/actions.csv')
+	transitions = read_csv_actions('samples/puzzle_mnist_3_3_36_20000_conv/all_actions.csv')
 	actions = generate_all_actions(transitions)
 	print len(actions)
 	pruned = prune_actions(actions)
@@ -230,10 +189,8 @@ def create_domain():
 
 
 
-
-
 def create_problem():
-	transitions = read_csv_actions('samples/puzzle_mnist_3_3_36_20000_conv/actions.csv')
+	transitions = read_csv_actions('samples/puzzle_mnist_3_3_36_20000_conv/all_actions.csv')
 	actions = generate_all_actions(transitions)
 	print len(actions)
 	pruned = prune_actions(actions)
@@ -247,4 +204,4 @@ def create_problem():
 	data.write(problem)
 	p, pre, eff = generate_action(transitions[0][0], transitions[0][1])
 	print eff
-create_domain()
+create_problem()
